53f9c0b6e60c3fc5ef0febd254b625cfd039fb2a
Michael Hennerich <michael.hennerich@analog.com>
2014-01-24 16:48:28 +0100

    fmcomms1-eeprom-cal: Add Support for Version 1 EEPROMS
    Signed-off-by: Michael Hennerich <michael.hennerich@analog.com>

--- main.c
+++ main.c
@@ -1,7 +1,7 @@
 /*
  * fmcomms1-eeprom-cal
  *
- * Copyright 2012 Analog Devices Inc.
+ * Copyright 2012-2014 Analog Devices Inc.
  *
  * Licensed under the GPL-2.
  */
@@ -28,34 +28,38 @@ void showusage(char *pgmname)
 	abort();
 }
 
-double fract_to_float(unsigned short val)
-{
-	double ret = 0;
-
-	if (val & 0x8000) {
-		ret = 1.0000;
-		val &= ~0x8000;
-	}
-
-	ret += (double)val / 0x8000;
-
-	return ret;
-}
-
 void print_entry(struct fmcomms1_calib_data *data)
 {
-	printf("Calibration Frequency:\t%d MHz\n", data->cal_frequency_MHz);
+	printf("Calibration Frequency: %d MHz\n", data->cal_frequency_MHz);
 
-	printf("DAC I Phase Adjust:\t\t%d\n", data->i_phase_adj);
-	printf("DAC Q Phase Adjust:\t\t%d\n", data->q_phase_adj);
-	printf("DAC I Offset:\t\t%u\n", data->i_dac_offset);
-	printf("DAC Q Offset:\t\t%u\n", data->q_dac_offset);
-	printf("DAC I Full Scale Adj:\t%u\n", data->i_dac_fs_adj);
-	printf("DAC Q Full Scale Adj:\t%u\n", data->q_dac_fs_adj);
-	printf("ADC I Offset:\t\t%d\n", data->i_adc_offset_adj);
-	printf("ADC Q Offset:\t\t%d\n", data->q_adc_offset_adj);
-	printf("ADC I Gain Adj:\t%f\n", fract_to_float(data->i_adc_gain_adj));
-	printf("ADC Q Gain Adj:\t%f\n", fract_to_float(data->q_adc_gain_adj));
+	printf("DAC I Phase Adjust:    %d\n", data->i_phase_adj);
+	printf("DAC Q Phase Adjust:    %d\n", data->q_phase_adj);
+	printf("DAC I Offset:          %u\n", data->i_dac_offset);
+	printf("DAC Q Offset:          %u\n", data->q_dac_offset);
+	printf("DAC I Full Scale Adj:  %u\n", data->i_dac_fs_adj);
+	printf("DAC Q Full Scale Adj:  %u\n", data->q_dac_fs_adj);
+	printf("ADC I Offset:          %d\n", data->i_adc_offset_adj);
+	printf("ADC Q Offset:          %d\n", data->q_adc_offset_adj);
+	printf("ADC I Gain Adj:        %f\n", fract1_15_to_float(data->i_adc_gain_adj));
+	printf("ADC Q Gain Adj:        %f\n", fract1_15_to_float(data->q_adc_gain_adj));
+}
+
+void print_entry_v1(struct fmcomms1_calib_data_v1 *data)
+{
+	printf("Calibration Frequency: %d MHz\n", data->cal_frequency_MHz);
+
+	printf("DAC I Phase Adjust:    %d\n", data->i_phase_adj);
+	printf("DAC Q Phase Adjust:    %d\n", data->q_phase_adj);
+	printf("DAC I Offset:          %d\n", data->i_dac_offset);
+	printf("DAC Q Offset:          %d\n", data->q_dac_offset);
+	printf("DAC I Full Scale Adj:  %u\n", data->i_dac_fs_adj);
+	printf("DAC Q Full Scale Adj:  %u\n", data->q_dac_fs_adj);
+	printf("ADC I Offset:          %d\n", data->i_adc_offset_adj);
+	printf("ADC Q Offset:          %d\n", data->q_adc_offset_adj);
+	printf("ADC I Gain Adj:        %f\n", fract1_1_14_to_float(data->i_adc_gain_adj));
+	printf("ADC Q Gain Adj:        %f\n", fract1_1_14_to_float(data->q_adc_gain_adj));
+	printf("ADC I Phase Adj:       %f\n", fract1_1_14_to_float(data->i_adc_phase_adj));
+
 }
 
 void store_entry_hw(struct fmcomms1_calib_data *data, unsigned tx, unsigned rx)
@@ -77,9 +81,47 @@ void store_entry_hw(struct fmcomms1_calib_data *data, unsigned tx, unsigned rx)
 		}
 		if (rx) {
 			fprintf(gp, "write cf-ad9643-core-lpc in_voltage0_calibbias %d\n", data->i_adc_offset_adj);
-			fprintf(gp, "write cf-ad9643-core-lpc in_voltage0_calibscale %f\n", fract_to_float(data->i_adc_gain_adj));
+			fprintf(gp, "write cf-ad9643-core-lpc in_voltage0_calibscale %f\n", fract1_15_to_float(data->i_adc_gain_adj));
+			fprintf(gp, "write cf-ad9643-core-lpc in_voltage0_calibphase %f\n", 0.0);
 			fprintf(gp, "write cf-ad9643-core-lpc in_voltage1_calibbias %d\n", data->q_adc_offset_adj);
-			fprintf(gp, "write cf-ad9643-core-lpc in_voltage1_calibscale %f\n", fract_to_float(data->q_adc_gain_adj));
+			fprintf(gp, "write cf-ad9643-core-lpc in_voltage1_calibscale %f\n", fract1_15_to_float(data->q_adc_gain_adj));
+			fprintf(gp, "write cf-ad9643-core-lpc in_voltage1_calibphase %f\n", 0.0);
+			fflush(gp);
+		}
+		pclose(gp);
+	} else {
+		fprintf (stderr, "popen iio_cmdsrv failed %d\n", errno);
+	}
+}
+
+void store_entry_hw_v1(struct fmcomms1_calib_data_v1 *data, unsigned tx,
+		       unsigned rx, unsigned short temp_calibbias)
+{
+
+	if (!(tx || rx))
+		return;
+
+	FILE *gp = popen("iio_cmdsrv","w");
+	if (gp) {
+
+		fprintf(gp, "write cf-ad9122-core-lpc in_temp0_calibbias %d\n", temp_calibbias);
+
+		if (tx) {
+			fprintf(gp, "write cf-ad9122-core-lpc out_voltage0_calibbias %d\n", data->i_dac_offset);
+			fprintf(gp, "write cf-ad9122-core-lpc out_voltage0_calibscale %d\n", data->i_dac_fs_adj);
+			fprintf(gp, "write cf-ad9122-core-lpc out_voltage0_phase %d\n", data->i_phase_adj);
+			fprintf(gp, "write cf-ad9122-core-lpc out_voltage1_calibbias %d\n", data->q_dac_offset);
+			fprintf(gp, "write cf-ad9122-core-lpc out_voltage1_calibscale %d\n", data->q_dac_fs_adj);
+			fprintf(gp, "write cf-ad9122-core-lpc out_voltage1_phase %d\n", data->q_phase_adj);
+			fflush(gp);
+		}
+		if (rx) {
+			fprintf(gp, "write cf-ad9643-core-lpc in_voltage0_calibbias %d\n", data->i_adc_offset_adj);
+			fprintf(gp, "write cf-ad9643-core-lpc in_voltage0_calibscale %f\n", fract1_1_14_to_float(data->i_adc_gain_adj));
+			fprintf(gp, "write cf-ad9643-core-lpc in_voltage0_calibphase %f\n", fract1_1_14_to_float(data->i_adc_phase_adj));
+			fprintf(gp, "write cf-ad9643-core-lpc in_voltage1_calibbias %d\n", data->q_adc_offset_adj);
+			fprintf(gp, "write cf-ad9643-core-lpc in_voltage1_calibscale %f\n", fract1_1_14_to_float(data->q_adc_gain_adj));
+			fprintf(gp, "write cf-ad9643-core-lpc in_voltage1_calibphase %f\n", 0.0);
 			fflush(gp);
 		}
 		pclose(gp);
@@ -91,7 +133,8 @@ void store_entry_hw(struct fmcomms1_calib_data *data, unsigned tx, unsigned rx)
 int main (int argc , char* argv[])
 {
 	FILE *fp;
-	struct fmcomms1_calib_data *data, *data2;
+	struct fmcomms1_calib_header_v1 *header;
+	char *buf;
 	int c, set_tx = 0, set_rx = 0, len, ind = 0;
 	int f = 0, delta, gindex;
 	int min_delta = 2147483647;
@@ -134,50 +177,93 @@ int main (int argc , char* argv[])
 		exit(EXIT_FAILURE);
 	}
 
-	data = data2 = malloc(FAB_SIZE_CAL_EEPROM);
-	if (data == NULL) {
+	buf = malloc(FAB_SIZE_CAL_EEPROM);
+	if (buf == NULL) {
 		perror("malloc");
 		fclose(fp);
 		exit(EXIT_FAILURE);
 	}
 
-	memset(data, 0, FAB_SIZE_CAL_EEPROM);
+	memset(buf, 0, FAB_SIZE_CAL_EEPROM);
 
-	len = fread(data, 1, FAB_SIZE_CAL_EEPROM, fp);
+	len = fread(buf, 1, FAB_SIZE_CAL_EEPROM, fp);
 	if (len != FAB_SIZE_CAL_EEPROM) {
 		perror("fread");
 		goto out;
 	}
 
-	do {
-		if (data->adi_magic0 != ADI_MAGIC_0 || data->adi_magic1 != ADI_MAGIC_1) {
-			fprintf (stderr, "invalid magic detected\n");
-			goto out;
-		}
-		if (data->version != ADI_VERSION(VERSION_SUPPORTED)) {
-			fprintf (stderr, "unsupported version detected %c\n", data->version);
-			goto out;
-		}
+	header = (struct fmcomms1_calib_header_v1 *) buf;
 
+	if (header->adi_magic0 != ADI_MAGIC_0 || header->adi_magic1 != ADI_MAGIC_1) {
+		fprintf (stderr, "invalid magic detected\n");
+		goto out;
+	}
 
-		if (f) {
-			delta = abs(f - data->cal_frequency_MHz);
-			if (delta < min_delta) {
-				gindex = ind;
-				min_delta = delta;
+	printf("\nFound Version %c \n", header->version);
+
+	if (header->version == ADI_VERSION(0)) {
+		struct fmcomms1_calib_data *data, *data2;
+		data = data2 = (struct fmcomms1_calib_data *)buf;
+
+		do {
+			if (data->adi_magic0 != ADI_MAGIC_0 || data->adi_magic1 != ADI_MAGIC_1) {
+				fprintf (stderr, "invalid magic detected\n");
+				goto out;
+			}
+			if (data->version != ADI_VERSION(VERSION_SUPPORTED)) {
+				fprintf (stderr, "unsupported version detected %c\n", data->version);
+				goto out;
 			}
 
-		} else {
-			printf("\n--- ENTRY %d ---\n", ind);
-			print_entry(data);
-		}
-		ind++;
-	} while (data++->next);
 
-	if (f) {
-		printf("\n--- Best match ENTRY %d ---\n", gindex);
-		print_entry(&data2[gindex]);
-		store_entry_hw(&data2[gindex], set_tx, set_rx);
+			if (f) {
+				delta = abs(f - data->cal_frequency_MHz);
+				if (delta < min_delta) {
+					gindex = ind;
+					min_delta = delta;
+				}
+
+			} else {
+				printf("\n--- ENTRY %d ---\n", ind);
+				print_entry(data);
+			}
+			ind++;
+		} while (data++->next);
+
+		if (f) {
+			printf("\n--- Best match ENTRY %d ---\n", gindex);
+			print_entry(&data2[gindex]);
+			store_entry_hw(&data2[gindex], set_tx, set_rx);
+		}
+
+	} else if (header->version == ADI_VERSION(1)) {
+		struct fmcomms1_calib_data_v1 *data =
+			(struct fmcomms1_calib_data_v1 *) (buf + sizeof(*header));
+
+		for (ind = 0; ind < header->num_entries; ind++) {
+
+			if (f) {
+				delta = abs(f - data[ind].cal_frequency_MHz);
+				if (delta < min_delta) {
+					gindex = ind;
+					min_delta = delta;
+				}
+
+			} else {
+				printf("\n--- ENTRY %d ---\n", ind);
+				print_entry_v1(&data[ind]);
+			}
+		};
+
+		if (f) {
+			printf("\n--- Best match ENTRY %d ---\n", gindex);
+			print_entry_v1(&data[gindex]);
+			store_entry_hw_v1(&data[gindex], set_tx, set_rx,
+					  header->temp_calibbias);
+		}
+	} else {
+		fprintf (stderr, "unsupported version detected %c\n", header->version);
+		goto out;
 	}
 
 	fclose(fp);
@@ -185,6 +271,6 @@ int main (int argc , char* argv[])
 
 out:
 	fclose(fp);
-	free(data);
+	free(buf);
 	exit(EXIT_FAILURE);
 }
